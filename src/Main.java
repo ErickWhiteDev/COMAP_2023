0803c3c25892b11aac0bc42b72fdde5dea01d4fb
Erick White <erickwhitebusiness@gmail.com>
2023-02-17 20:03:28 -0700

    Implemented achievement propagation
    + Implemented vertex achievement metric propagation
    
    * Added weighted graph utilities class
    * Organized and documented code
    
    - Removed unnecessary tests

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
@@ -60,151 +59,11 @@ public class Main {
 
         WeightedGraph<Vertex> goals = new WeightedGraph<>(goalList);
 
-        Vertex test1 = new Vertex("test vertex 1");
-        Vertex test2 = new Vertex("test vertex 2");
-        Vertex test3 = new Vertex("test vertex 3");
-        Vertex test4 = new Vertex("test vertex 4");
-
-        WeightedGraph<Vertex> testGraph = new WeightedGraph<>(List.of(test1, test2, test3,  test4));
-
-        testGraph.addEdge(test1, test2, 0.5);
-        testGraph.addEdge(test1, test3, 0.6);
-        testGraph.addEdge(test1, test4, -.4);
-        testGraph.addEdge(test2, test3, 0.1);
-        testGraph.addEdge(test2, test4, 0);
-        testGraph.addEdge(test3, test4, -.2);
-
         // Fill in weights from CSV data
         for (int i = 0; i < 17; i++) {
             for (int j = 0; j < i; j++) {
                 goals.addEdge(i, j, Double.parseDouble(weightsString[i][j]));
             }
         }
-    }
-
-    
-    /**
-     * <h1>Display All Achievements</h1>
-     * displayAllAchievements() displays the current achievement value of each vertex. Useful for testing and debugging.
-     * 
-     * @author Elizabeth Cutting
-     * @param graph network whose achievement values will be displayed
-     */
-    protected static void displayAllAchievements(WeightedGraph<Vertex> graph) {
-    	//print out the current achievement for the base vertex
-    	System.out.println(graph.vertexAt(0).toString() + " " + graph.vertexAt(0).getAchievement());
-    	//print out the achievement for the rest of the vertices
-        for(Vertex v : graph.neighborsOf(graph.vertexAt(0))) {
-        	System.out.println(v.toString() + " " + v.getAchievement());
-        }
-    }
-    
-    /**
-     * <h1>Propagate Achievement</h1>
-     * propagateAchievement takes the <b>achievement metric</b> of one vertex in a graph and propagates it to other vertices.
-     * The relation between propagation and distance from the source vertex is inversely proportional to the distance (nonlinearly)
-     * between the source and the node experiencing the effect. Propagation can end up affecting the original node
-     * due to connections in the graph.
-     *
-     * @author Erick White
-     * @param parent vertex that has undergone a change in achievement score
-     * @param graph network through which to propagate
-     * @param curr current depth of propagation
-     * @param depth maximum propagation depth
-     */
-    private static void propagateAchievement(Vertex parent, WeightedGraph<Vertex> graph, double curr, int depth) {
-        for (Vertex v : graph.neighborsOf(parent)) {
-            /* Increment achievement by amount proportional to
-             * the product of achievement scores times the weights of the connections
-             * and inversely proportional to distance (nonlinearly)
-             */
-            v.setAchievement(v.getAchievement() + (v.getAchievement() * graph
-                    .edgesOf(parent)
-                    .get((graph.indexOf(v) < graph.indexOf(parent)) ? graph.indexOf(v) : graph.indexOf(v) - 1)
-                    .weight) / Math.pow(16, curr - .5)
-            );
-
-            if (curr != depth) {
-                propagateAchievement(v, graph, curr + 1, depth);
-            }
-        }
-    }
-
-    /**
-     * <h1>Get Goal Priority</h1>
-     * Find the priority level of a node based on the weights of its connections.
-     * The priority is inversely proportional to the layer depth.
-     *
-     * @author Erick White
-     * @param parent node for which priority must be calculated
-     * @param graph network parent node belongs to
-     * @param depth maximum summation depth level
-     * @return priority level of node
-     */
-    private static double getGoalPriority(Vertex parent, WeightedGraph<Vertex> graph, int layerSize, int depth) {
-        ArrayList<ArrayList<Vertex>> layers = new ArrayList<>();
-        double priority = 0;
-        double layerWeight;
-
-        for (int i = 0; i < depth; i++) {
-            layers.add(new ArrayList<>());
-            getLayer(parent, graph, 0, i, layers.get(i));
-            layerWeight = sumLayerWeights(layers.get(i), graph) / Math.pow(layerSize, i + 1);
-
-            priority += (layerWeight / Math.abs(layerWeight)) / (Math.sqrt(i + 1) * Math.pow(1 + layerWeight, i + 1));
-        }
-
-        return priority;
-    }
-
-    /**
-     * <h1>Sum Layer Weights</h1>
-     * Given an arbitrary layer from the getLayer() function, sum up the weights of all vertices it contains
-     *
-     * @param layer layer with weights being summed
-     * @param graph network to which layer belongs
-     * @return sum of all weights in a layer
-     */
-    private static double sumLayerWeights(ArrayList<Vertex> layer, WeightedGraph<Vertex> graph) {
-        double sum = 0;
-
-        for (Vertex v : layer) {
-            for (Vertex n : graph.neighborsOf(v)) {
-                sum += graph
-                        .edgesOf(v)
-                        .get((graph.indexOf(n) < graph.indexOf(v)) ? graph.indexOf(n) : graph.indexOf(n) - 1)
-                        .weight;
-            }
-        }
-
-        return sum;
-    }
-
-    /**
-     * <h1>Get Layer</h1>
-     * Given a central node, get all nodes a certain depth away from it
-     *
-     * @author Erick White
-     * @param parent central node (propagates outwards from here)
-     * @param graph network node belongs to
-     * @param curr current layer depth being checked
-     * @param depth layer desired
-     * @param layer ArrayList of all vertices on the final layer
-     */
-    private static void getLayer(Vertex parent, WeightedGraph<Vertex> graph, int curr, int depth, ArrayList<Vertex> layer) {
-        if (depth == 0) {
-            layer.add(parent);
-        }
-        else {
-            for (Vertex v : graph.neighborsOf(parent)) {
-                if (curr == depth - 1) {
-                    layer.add(v);
-                }
-                else {
-                    getLayer(v, graph, curr + 1, depth, layer);
-                }
-            }
-        }
-
     }
 }
---
+++ src/WeightedGraphUtilities.java
@@ -0,0 +1,210 @@
+import java.util.ArrayList;
+
+public class WeightedGraphUtilities {
+    /**
+     * <h1>Get Layer</h1>
+     * Given a central node, get all nodes a certain depth away from it
+     *
+     * @author Erick White
+     * @param parent central node (propagates outwards from here)
+     * @param graph network node belongs to
+     * @param curr current layer depth being checked
+     * @param depth layer desired
+     * @param layer ArrayList of all vertices on the final layer
+     */
+    public static void getLayer(Vertex parent, WeightedGraph<Vertex> graph, int curr, int depth, ArrayList<Vertex> layer) {
+        if (depth == 0) {
+            layer.add(parent);
+        }
+        else {
+            for (Vertex v : graph.neighborsOf(parent)) {
+                if (curr == depth - 1) {
+                    layer.add(v);
+                }
+                else {
+                    getLayer(v, graph, curr + 1, depth, layer);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * <h1>Get Layer Weights</h1>
+     * Returns a vector of the <b>weights of connections</b> of a given layer.
+     *
+     * @author Erick White
+     * @param layer layer from which weights are being retrieved
+     * @param graph network to which layer belongs
+     * @return ArrayList of doubles containing all weights of connections on a layer
+     */
+    public static ArrayList<Double> getLayerWeights(ArrayList<Vertex> layer, WeightedGraph<Vertex> graph) {
+        ArrayList<Double> weights = new ArrayList<>();
+
+        for (Vertex v : layer) {
+            for (Vertex n : graph.neighborsOf(v)) {
+                weights.add(graph
+                        .edgesOf(v)
+                        .get((graph.indexOf(n) < graph.indexOf(v)) ? graph.indexOf(n) : graph.indexOf(n) - 1)
+                        .weight
+                );
+            }
+        }
+
+        return weights;
+    }
+
+    /**
+     * <h1>Get Layer Achievements</h1>
+     * Returns a vector of the <b>achievement metrics</b> of a given layer.
+     *
+     * @author Erick White
+     * @param layer layer from which achievements are being retrieved
+     * @param graph network to which layer belongs
+     * @return ArrayList of doubles containing all achievements on a layer
+     */
+    public static ArrayList<Double> getLayerAchievements(ArrayList<Vertex> layer, WeightedGraph<Vertex> graph) {
+        ArrayList<Double> achievements = new ArrayList<>();
+
+        for (Vertex v : layer) {
+            for (Vertex n : graph.neighborsOf(v)) {
+                achievements.add(n.getAchievement());
+            }
+        }
+
+        return achievements;
+    }
+
+    /**
+     * <h1>Dot Product</h1>
+     * Takes the dot product of two vectors of doubles.
+     *
+     * @author Erick White
+     * @param arr1 first vector
+     * @param arr2 second vector
+     * @return dot product of two input vectors
+     */
+    public static double dotProduct(ArrayList<Double> arr1, ArrayList<Double> arr2) {
+        double sum = 0;
+
+        for (int i = 0; i < arr1.size(); i++) {
+            sum += arr1.get(i) * arr2.get(i);
+        }
+
+        return sum;
+    }
+
+    /**
+     * <h1>Sum Array</h1>
+     * Sums contents of an array.
+     *
+     * @author Erick White
+     * @param arr array to sum (doubles)
+     * @return sum of elements in array
+     */
+    public static double sumArray(ArrayList<Double> arr) {
+        double sum = 0;
+
+        for (Double d : arr) {
+            sum += d;
+        }
+
+        return sum;
+    }
+
+    /**
+     * <h1>Get Goal Priority</h1>
+     * Find the priority level of a node based on the weights of its connections.
+     * The priority is inversely proportional to the layer depth.
+     *
+     * @author Erick White
+     * @param parent node for which priority must be calculated
+     * @param graph network parent node belongs to
+     * @param layerSize number of vertices in layer
+     * @param depth maximum summation depth level
+     * @return priority level of node
+     */
+    public static double getGoalPriority(Vertex parent, WeightedGraph<Vertex> graph, int layerSize, int depth) {
+        ArrayList<ArrayList<Vertex>> layers = new ArrayList<>();
+        double priority = 0;
+        double layerWeight;
+
+        for (int i = 0; i < depth; i++) {
+            layers.add(new ArrayList<>());
+            getLayer(parent, graph, 0, i, layers.get(i));
+            layerWeight = sumArray(getLayerWeights(layers.get(i), graph)) / Math.pow(layerSize, i + 1);
+
+            priority += (layerWeight / Math.abs(layerWeight)) / (Math.sqrt(i + 1) * Math.pow(1 + layerWeight, i + 1));
+        }
+
+        return priority;
+    }
+
+    /**
+     * <h1>Get Neighborhood Achievement</h1>
+     * getNeighborhoodAchievement takes the <b>achievement metrics</b> of surrounding vertex in a graph and propagates it to the parent vertex.
+     * The relation between propagation and distance from the source vertex is inversely proportional to the distance (nonlinearly)
+     * between the source and the node experiencing the effect. Propagation can end up depending on the original node
+     * due to connections in the graph.
+     *
+     * @author Erick White
+     * @param parent vertex that has undergone a change in achievement score
+     * @param graph network through which to propagate
+     * @param layerSize size of one layer of vertices
+     * @param depth maximum propagation depth
+     * @param k coefficient determining fall-off rate of propagation
+     */
+    public static double getNeighborhoodAchievement(Vertex parent, WeightedGraph<Vertex> graph, int layerSize, int depth, double k) {
+        ArrayList<ArrayList<Vertex>> layers = new ArrayList<>();
+        double weightedAchievementProduct = 0;
+
+        for (int i = 0; i < depth; i++) {
+            layers.add(new ArrayList<>());
+            getLayer(parent, graph, 0, i, layers.get(i));
+
+            weightedAchievementProduct += dotProduct(getLayerWeights(layers.get(i), graph), getLayerAchievements(layers.get(i), graph)) / Math.pow(layerSize, i + 1 - k);
+        }
+
+        return parent.getAchievement() + weightedAchievementProduct;
+    }
+
+    /**
+     * <h1>Update Achievements</h1>
+     * updateAchievements takes all the vertices in a network and adjusts the values of the achievements
+     * corresponding to their weighted connections. Propagation can end up depending on the original node
+     * due to connections in the graph.
+     *
+     * @author Erick White
+     * @param graph graph being updated
+     * @param layerSize size of one layer of vertices
+     * @param depth maximum search depth for updates
+     * @param k coefficient determining fall-off rate of achievement propagation
+     */
+    public static void updateAchievements(WeightedGraph<Vertex> graph, int layerSize, int depth, double k) {
+        double[] newAchievements = new double[graph.getVertexCount()];
+
+        for (int i = 0; i < graph.getVertexCount(); i++) {
+            newAchievements[i] = getNeighborhoodAchievement(graph.vertexAt(i), graph, layerSize, depth, k);
+        }
+
+        for (int i = 0; i < graph.getVertexCount(); i++) {
+            graph.vertexAt(i).setAchievement(newAchievements[i]);
+        }
+    }
+
+    /**
+     * <h1>Display All Achievements</h1>
+     * displayAllAchievements() displays the current achievement value of each vertex. Useful for testing and debugging.
+     *
+     * @author Elizabeth Cutting
+     * @param graph network whose achievement values will be displayed
+     */
+    public static void displayAllAchievements(WeightedGraph<Vertex> graph) {
+        //print out the current achievement for the base vertex
+        System.out.println(graph.vertexAt(0).toString() + " " + graph.vertexAt(0).getAchievement());
+        //print out the achievement for the rest of the vertices
+        for(Vertex v : graph.neighborsOf(graph.vertexAt(0))) {
+            System.out.println(v.toString() + " " + v.getAchievement());
+        }
+    }
+}
