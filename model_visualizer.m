e0b942c739f5f232251f531ce856ac83efa6fe56
Erick White <erickwhitebusiness@gmail.com>
2023-02-19 12:35:15 -0700

    Added visualizations for network structure and decay patterns

---
+++ model_visualizer.m
@@ -0,0 +1,42 @@
+graph = readmatrix("src/weights.csv");
+pad_bottom = zeros([1 17]);
+pad_side = zeros([18 1]);
+names = readlines("names.txt");
+
+adjusted_bottom = [graph ; pad_bottom];
+adjusted = [adjusted_bottom pad_side];
+
+%% Connection Weights
+figure(1);
+pcolor(adjusted);
+
+colorbar;
+axis square;
+set(gca, 'xtick', (1:numel(names)) + .25, 'xticklabels', names);
+set(gca, 'ytick', (1:numel(names)) + 0.5, 'yticklabels', names);
+set(gca, 'fontsize', 14);
+xtickangle(60);
+title("Connection Weight Strengths");
+
+%% Achievement and Priority
+x_A = 0:.01:5;
+x_P = 1:.01:5;
+k = 2.56;
+A = 1 ./ (16 * k).^x_A;
+P = 1 ./ (sqrt(x_P) .* (1 - 1 ./ (16.^x_P)).^x_P);
+
+hold on;
+pA = plot(x_A, A);
+pP = plot(x_P, P);
+pA.LineWidth = 2;
+pP.LineWidth = 2;
+
+set(gca, 'fontsize', 14);
+set(gca, 'LineWidth', 2);
+set(gca, 'xtick', 1:5);
+grid on;
+title("Achievement and Priority Decay With Layer Depth");
+xlabel("Layer Depth");
+ylabel("Value");
+
+legend("Achievement Score", "Priority Score");
